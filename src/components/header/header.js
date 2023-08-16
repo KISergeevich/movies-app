342a6ad321f707600530bd25e7824f4f8cf86312
Kalinin Ivan <indimion57@gmail.com>
2023-08-16 16:33:43 +0300

    redact css  properties in hedder component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -8,17 +8,20 @@ export default class Header extends Component {
       key: '1',
       label: 'Search',
       children: (
-        <input
-          className="header__search"
-          placeholder="Type to search..."
-          onChange={(event) => this.searchChanged(event)}
-          type="text"
-        />
+        <div className="header__box">
+          <input
+            className="header__search"
+            placeholder="Type to search..."
+            onChange={(event) => this.searchChanged(event)}
+            type="text"
+          />
+        </div>
       ),
     },
     {
       key: '2',
       label: 'Rated',
+      children: <div className="header__rated" onChange={(event) => this.searchChanged(event)} />,
     },
   ]
 
