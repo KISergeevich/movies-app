316f2e707c4b13cc37838d68e6a339fdae10e589
Kalinin Ivan <indimion57@gmail.com>
2023-10-01 16:01:22 +0300

    switch autorisation on apiKey

--- src/services/api-movie-db.js
+++ src/services/api-movie-db.js
@@ -3,6 +3,8 @@ export default class ApiMovieDB {
   auth =
     'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMTExZjU5Yzg2NGJiZTA0ZTVkMTE2ZjVhNWJmYjFjYiIsInN1YiI6IjY0YWQwNzU5YjY4NmI5MDEyZjg4NjNmNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.A1OX3DUjFn8jWHt82mVoDiBy4-NohG23I3uQdXr7qYw'
 
+  apiKey = '1111f59c864bbe04e5d116f5a5bfb1cb'
+
   guestSessionId = undefined
 
   async createGuestSession() {
@@ -10,10 +12,12 @@ export default class ApiMovieDB {
       method: 'GET',
       headers: {
         accept: 'application/json',
-        Authorization: `Bearer ${this.auth}`,
       },
     }
-    const response = await fetch('https://api.themoviedb.org/3/authentication/guest_session/new', options)
+    const response = await fetch(
+      `https://api.themoviedb.org/3/authentication/guest_session/new?api_key=${this.apiKey}`,
+      options
+    )
     const json = await response.json()
     const { success, guest_session_id } = json
     if (success) {
@@ -26,11 +30,10 @@ export default class ApiMovieDB {
       method: 'GET',
       headers: {
         accept: 'application/json',
-        Authorization: `Bearer ${this.auth}`,
       },
     }
     const response = await fetch(
-      `https://api.themoviedb.org/3/guest_session/${this.guestSessionId}/rated/movies?language=en-US&page=1&sort_by=created_at.asc`,
+      `https://api.themoviedb.org/3/guest_session/${this.guestSessionId}/rated/movies?api_key=${this.apiKey}&language=en-US&page=1&sort_by=created_at.asc`,
       options
     )
     const json = await response.json()
@@ -48,14 +51,13 @@ export default class ApiMovieDB {
       headers: {
         accept: 'application/json',
         'Content-Type': 'application/json;charset=utf-8',
-        Authorization: `Bearer ${this.auth}`,
       },
       body: JSON.stringify({
         value: rating,
       }),
     }
     const response = await fetch(
-      `https://api.themoviedb.org/3/movie/${movieId}/rating?guest_session_id=${this.guestSessionId}`,
+      `https://api.themoviedb.org/3/movie/${movieId}/rating?api_key=${this.apiKey}&guest_session_id=${this.guestSessionId}`,
       options
     )
     if (response.ok) {
@@ -69,10 +71,12 @@ export default class ApiMovieDB {
       method: 'GET',
       headers: {
         accept: 'application/json',
-        Authorization: `Bearer ${this.auth}`,
       },
     }
-    const response = await fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', options)
+    const response = await fetch(
+      `https://api.themoviedb.org/3/genre/movie/list?api_key=${this.apiKey}&language=en`,
+      options
+    )
     const json = await response.json()
     const { genres } = json
     return genres
@@ -83,13 +87,12 @@ export default class ApiMovieDB {
       method: 'GET',
       headers: {
         accept: 'application/json',
-        Authorization: `Bearer ${this.auth}`,
       },
     }
     let response
     try {
       response = await fetch(
-        `https://api.themoviedb.org/3/search/movie?query=${search}&include_adult=false&language=en-US&page=${page}`,
+        `https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=${search}&include_adult=false&language=en-US&page=${page}`,
         options
       )
     } catch {
